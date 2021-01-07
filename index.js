c383df2e8dad47941794d16a2f5c45bdf8df6124
Randi Schreiner <mimirandi21@gmail.com>
2021-01-07 12:38:43 -0600

    commit tree 1d114b21b9c376039f449cbebe0e0c8cbfd4dd69

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const people = ["Ada", "Brendan", "Ali"];
+const reason = "birthday";
+
+function writeCards(people, reason) {
+	let message = [];
+	for (let i = 0; i < people.length; i++) {
+		message[i] = `Thank you, ${people[i]}, for the wonderful ${reason} gift!`;
+	}
+	return message;
+}
+writeCards(people, reason);
+
+function countDown(number) {
+	let i = number;
+	while (i >= 0) {
+		console.log(i);
+		i--;
+	}
+}
+countDown(10);
